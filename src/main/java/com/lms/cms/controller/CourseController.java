5162b8f18b4895276fd61c4c50aea7680b915de0
dainb <dai15081999@gmail.com>
2026-06-27 01:04:43 +0700

    Initial commit

---
+++ src/main/java/com/lms/cms/controller/CourseController.java
@@ -0,0 +1,46 @@
+package com.lms.cms.controller;
+
+import com.lms.cms.entity.Course;
+import com.lms.cms.service.CourseService;
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping
+    public Course createCourse(@RequestBody Course course) {
+        return courseService.createCourse(course);
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourseById(@PathVariable Long id) {
+        return courseService.getCourseById(id);
+    }
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @PatchMapping("/{id}")
+    public Course updateCoursePartially(@PathVariable Long id,@RequestBody Course updatedCourse) {
+        return courseService.updateCoursePartially(id, updatedCourse);
+    }
+
+    @PutMapping("/{id}")
+    public Course updateCourseCompletely(@PathVariable Long id,@RequestBody Course updatedCourse) {
+        return courseService.updateCourseCompletely(id, updatedCourse);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCourse(@PathVariable Long id) {
+        return courseService.deleteCourse(id);
+    }
+}
---
+++ src/main/java/com/lms/cms/service/CourseService.java
@@ -0,0 +1,58 @@
+package com.lms.cms.service;
+
+import com.lms.cms.entity.Course;
+import com.lms.cms.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+    private final CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Course getCourseById(Long id) {
+        return courseRepository.findById(id).get();
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course updateCoursePartially(Long id, Course updatedCourse) {
+        Course existingCourse = courseRepository.findById(id).get();
+        if(updatedCourse.getName() != null) {
+            existingCourse.setName(updatedCourse.getName());
+        }
+        if(updatedCourse.getDescription() != null) {
+            existingCourse.setDescription(updatedCourse.getDescription());
+        }
+        if(updatedCourse.getPrice() != null) {
+            existingCourse.setPrice(updatedCourse.getPrice());
+        }
+
+        return courseRepository.save(existingCourse);
+    }
+
+    public Course updateCourseCompletely(Long id, Course updatedCourse) {
+        Course existingCourse = courseRepository.findById(id).get();
+
+        existingCourse.setName(updatedCourse.getName());
+        existingCourse.setDescription(updatedCourse.getDescription());
+        existingCourse.setPrice(updatedCourse.getPrice());
+
+        return courseRepository.save(existingCourse);
+    }
+
+    public String deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+        return "Course deleted successfully";
+    }
+}
